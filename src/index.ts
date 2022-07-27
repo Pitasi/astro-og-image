46a7f5990172b51176a18ec486d540768a8c46c7
Antonio Pitasi <antonio@pitasi.dev>
2022-07-27 16:48:05 +0200

    experiments

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import * as fs from "fs";
 import puppeteer from "puppeteer";
 import { fileURLToPath } from "node:url";
 import type { AstroIntegration, RouteData } from "astro";
+import httpServer from "http-server";
 
 export default function astroOGImage({
   config,
@@ -12,48 +13,42 @@ export default function astroOGImage({
     name: "astro-og-image",
     hooks: {
       "astro:build:done": async ({ dir, routes }) => {
-        let path = config.path;
-        // Filters all the routes that need OG image
-        let filteredRoutes = routes.filter((route: RouteData) =>
-          route?.pathname?.includes(path)
-        );
-
         // Creates a directory for the images if it doesn't exist already
-        let directory = fileURLToPath(new URL(`./assets/${path}`, dir));
-        if (!fs.existsSync(directory)) {
-          fs.mkdirSync(directory);
+        let outputDir = fileURLToPath(new URL(`./assets/og`, dir));
+        if (!fs.existsSync(outputDir)) {
+          fs.mkdirSync(outputDir);
         }
+
+        let inputDir = fileURLToPath(new URL(`./og_`, dir));
+        const inputPaths = fs.readdirSync(inputDir);
+        const h = httpServer.createServer({
+          root: fileURLToPath(new URL(".", dir)),
+        });
+        h.listen(9099, "0.0.0.0");
 
         const browser = await puppeteer.launch();
-        for (const route of filteredRoutes) {
-          // Gets the title
-          const data = fs.readFileSync(
-            route?.distURL?.pathname as any,
-            "utf-8"
-          ) as any;
-          let title = await data.match(/<title[^>]*>([^<]+)<\/title>/)[1];
 
-          // Get the html
-          const html = fs
-            .readFileSync("og-image.html", "utf-8")
-            .toString()
-            .replace("@title", title);
+        const genPromises = inputPaths.map((route) => gen(dir, browser, route));
+        await Promise.all(genPromises);
 
-          const page = await browser.newPage();
-          await page.setContent(html);
-          await page.waitForNetworkIdle();
-          await page.setViewport({
-            width: 1200,
-            height: 630,
-          });
-
-          await page.screenshot({
-            path: fileURLToPath(new URL(`./assets${route.pathname}.png`, dir)),
-            encoding: "binary",
-          });
-        }
         await browser.close();
+        h.close();
       },
     },
   };
+}
+
+async function gen(dir: URL, browser: puppeteer.Browser, route: string) {
+  const page = await browser.newPage();
+  await page.goto(`http://localhost:9099/og_/${route}`);
+  await page.waitForNetworkIdle();
+  await page.setViewport({
+    width: 1200,
+    height: 630,
+  });
+
+  await page.screenshot({
+    path: fileURLToPath(new URL(`./assets/og/${route}.png`, dir)),
+    encoding: "binary",
+  });
 }
